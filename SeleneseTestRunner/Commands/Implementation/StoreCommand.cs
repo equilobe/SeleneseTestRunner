77069505b7c6d1e8f1391182ee1cd403fbb55f61
Radu Ungureanu <radu.ungureanu@equilobe.com>
2016-02-24 10:32:36 +0200

    Added new command to Store variables

---
+++ SeleneseTestRunner/Commands/Implementation/StoreCommand.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium;
+using SeleneseTestRunner.Suites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleneseTestRunner.Commands.Implementation
+{
+    public class StoreCommand : ICommand
+    {
+        public CommandResult Execute(IWebDriver driver, CommandDesc command)
+        {
+            try
+            {
+                var result = new CommandResult { Command = command };
+                SuiteExecutor.StoreValue(command.Parameter, command.Selector);
+                return result;
+            }
+            catch (Exception ex)
+            {
+                return new CommandResult
+                {
+                    Command = command,
+                    HasError = true,
+                    Comments = ex.Message
+                };
+            }
+        }
+    }
+}
